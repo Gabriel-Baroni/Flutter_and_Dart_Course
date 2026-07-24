138a595f37a2a76c7390b87c145103ce16f8b591
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2026-07-24 14:10:45 -0300

    Add dart_Libs

--- module1/main.dart
+++ module1/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main(){
-    runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Hello World Web',
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Text('Hello World Web App'),
-                ),
-                body: Center(
-                    child: Text(
-                        'Hello Flutter',
-                        style: TextStyle(fontSize: 24),
-                    ),
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Web',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello World Web App')),
+        body: Center(
+          child: Text('Hello Flutter', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    );
+  }
 }
